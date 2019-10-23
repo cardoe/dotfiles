ca9de222e3b4a9b40f53d754dd447cfda51a51c9
Doug Goldstein <cardoe@cardoe.com>
2019-10-23 09:43:32 -0500

    ipython: configure autoreload by default

---
+++ home/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,5 @@
++## A list of dotted module names of IPython extensions to load.
++c.InteractiveShellApp.extensions = ['autoreload']
++
++## lines of code to run at IPython startup.
++c.InteractiveShellApp.exec_lines = ['%autoreload 2']

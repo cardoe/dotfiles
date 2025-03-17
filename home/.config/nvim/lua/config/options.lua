0a4d04f99a6f50812a8b05896ecc2da26881a401
Doug Goldstein <cardoe@cardoe.com>
2025-03-17 15:08:50 -0500

    vim: disable auto-formatting

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,3 +1,5 @@
 -- LSP Server to use for Python.
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
+-- Disable auto-formatting to not battle upstream code style
+vim.g.autoformat = false

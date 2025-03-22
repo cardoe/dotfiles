78ad96c8243c60fbd8d5b76d8adba33b1d4b104d
Doug Goldstein <cardoe@cardoe.com>
2025-03-22 11:16:21 -0500

    vim: basedpyright is so loud

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 -- LSP Server to use for Python.
-vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
 -- Disable auto-formatting to not battle upstream code style
 vim.g.autoformat = false
